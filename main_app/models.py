b56bbd61e5692872347a026dce78ff0d3299ccc8
jlandis320 <jreynoldslandis@gmail.com>
2022-11-09 20:26:45 -0500

    adds titles

--- main_app/models.py
+++ main_app/models.py
@@ -9,9 +9,17 @@ class Character(models.Model):
   age = models.CharField(max_length=20)
   pronoun = models.CharField(max_length=10)
 
-
   def __str__(self):
     return self.name
 
   def get_absolute_url(self):
     return reverse('characters_detail', kwargs={'character_id': self.id})
+
+
+class Title(models.Model):
+  title = models.CharField(max_length=100)
+  date = models.DateField('Release Date')
+  character = models.ForeignKey(Character, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return f"{self.title} ({self.date})"
